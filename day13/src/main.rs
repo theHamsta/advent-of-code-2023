078d1b5d894dca0888e62a7835552f19cc66f4ea
Stephan Seitz <stephan.seitz@fau.de>
2023-12-14 07:05:00 +0100

    Part1

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,115 +1,120 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use itertools::Itertools;
 
-fn calc_reflection_sum(input: &[Vec<char>]) -> (u64, Vec<u64>, Vec<u64>) {
-    let mut sum = 0u64;
-    let mut x_reflections = Vec::new();
-    let mut y_reflections = Vec::new();
+fn tilt(input: &mut Vec<Vec<char>>, direction: (i64, i64)) {
+    let y_iter = (0..input.len()).map(|x| x as i64);
+    let x_iter = (0..input[0].len()).map(|x| x as i64);
 
-    for y in 0..(input.len() as i64) {
-        let mut is_symetric = true;
-        let mut has_one_comparision = false;
-        'outer: for y_ in 1..(input.len() as i64) {
-            let low_y = y - y_ + 1;
-            let high_y = y + y_;
-            if low_y < 0 || high_y >= (input.len() as i64) {
-                continue;
-            }
-            for x in 0..(input[0].len() as i64) {
-                has_one_comparision = true;
-                if input[low_y as usize][x as usize] != input[high_y as usize][x as usize] {
-                    is_symetric = false;
-                    break 'outer;
+    let it: Box<dyn Iterator<Item = (i64, i64)>> = match direction {
+        (1, 0) => Box::new(y_iter.cartesian_product(x_iter.rev())),
+        (-1, 0) => Box::new(y_iter.cartesian_product(x_iter)),
+        (0, 1) => Box::new(y_iter.rev().cartesian_product(x_iter)),
+        (0, -1) => Box::new(y_iter.cartesian_product(x_iter)),
+        _ => unreachable!(),
+    };
+
+    for (y, x) in it.into_iter() {
+        //println!("{y} {x}");
+        if input[y as usize][x as usize] == 'O' {
+            let mut shifted = (x, y);
+            let mut last = (x, y);
+            loop {
+                shifted = (shifted.0 + direction.0, shifted.1 + direction.1);
+                let (sx, sy) = shifted;
+                if (sx < 0 || sx >= input[0].len() as i64) || (sy < 0 || sy >= input.len() as i64) {
+                    break;
                 }
+                if input[shifted.1 as usize][shifted.0 as usize] != '.' {
+                    break;
+                }
+
+                input[shifted.1 as usize][shifted.0 as usize] = 'O';
+                input[last.1 as usize][last.0 as usize] = '.';
+                last = shifted;
             }
-        }
-        if is_symetric && has_one_comparision {
-            sum += (y as u64 + 1) * 100;
-            y_reflections.push(y as u64 + 1);
+            //println!("Shifted from {x},{y} to {},{}", last.0, last.1);
         }
     }
+}
 
-    for x in 0..(input[0].len() as i64) {
-        let mut is_symetric = true;
-        let mut has_one_comparision = false;
-        'outer: for x_ in 1..(input.len() as i64) {
-            let low_x = x - x_ + 1;
-            let high_x = x + x_;
-            if low_x < 0 || high_x >= (input[0].len() as i64) {
-                continue;
-            }
-            for y in 0..(input.len() as i64) {
-                has_one_comparision = true;
-                if input[y as usize][low_x as usize] != input[y as usize][high_x as usize] {
-                    is_symetric = false;
-                    break 'outer;
-                }
-            }
+fn plot(input: &Vec<Vec<char>>) {
+    for y in 0..input.len() {
+        for x in 0..input[0].len() {
+            print!("{}", input[y][x]);
         }
-        if is_symetric && has_one_comparision {
-            sum += x as u64 + 1;
-
-            x_reflections.push(x as u64 + 1);
-        }
+        println!();
     }
-    (sum, x_reflections, y_reflections)
+    println!();
 }
 
 fn main() -> anyhow::Result<()> {
     let raw_input = include_str!("../input");
-    //let raw_input = include_str!("../example1");
+    let raw_input = include_str!("../example1");
 
     let input = raw_input
         .split("\n\n")
-        .map(|l| {
-            l.lines()
+        .map(|section| {
+            section
+                .lines()
                 .map(|l| l.chars().collect_vec())
-                .collect::<Vec<Vec<char>>>()
+                .collect_vec()
         })
         .collect_vec();
+    //plot(&input);
+    //println!();
+    let mut shifted = input.clone();
 
-    let part1: u64 = input.iter().map(|i| calc_reflection_sum(i).0).sum();
-    dbg!(&part1);
+    for shifted in shifted.iter_mut() {
+        tilt(shifted, (0, -1));
+    }
+    //plot(&shifted);
 
-    let part2: u64 = input
-        .clone()
-        .iter_mut()
-        .map(|i| {
-            let (_, orig_x, orig_y) = calc_reflection_sum(i);
-
-            for y in 0..i.len() {
-                for x in 0..i[0].len() {
-                    let orig = i[y][x];
-                    i[y][x] = match i[y][x] {
-                        '#' => continue,
-                        '.' => '#',
-                        _ => unreachable!(),
-                    };
-                    let (_, new_x, new_y) = calc_reflection_sum(i);
-                    //dbg!(&new_x);
-                    //dbg!(&new_y);
-                    let is_smudge = (!new_x.is_empty() || !new_y.is_empty())
-                        && (new_x != orig_x || new_y != orig_y);
-
-                    if is_smudge {
-                        let old_x: HashSet<_> = orig_x.iter().collect();
-                        let old_y: HashSet<_> = orig_y.iter().collect();
-                        let mut sum: u64 = new_x.iter().filter(|x| !old_x.contains(x)).sum();
-                        sum += new_y.iter().filter(|y| !old_y.contains(y)).sum::<u64>() * 100;
-                        return sum;
-                    }
-
-                    i[y][x] = orig;
+    let mut sum = 0;
+    for shifted in shifted.iter() {
+        for y in 0..shifted.len() {
+            for x in 0..shifted[0].len() {
+                if shifted[y][x] == 'O' {
+                    sum += shifted.len() - y;
                 }
             }
+        }
+    }
+    let part1 = sum;
+    dbg!(&part1);
 
-            println!();
-            panic!("No smudge found")
-        })
-        .sum();
+    let mut shifted = input.clone();
+
+    let num_iterations = 3;
+    //let num_iterations = 1000000000;
+    for shifted in shifted.iter_mut() {
+        plot(shifted);
+        let before_it = shifted.clone();
+        for i in 0..num_iterations {
+            tilt(shifted, (0, -1));
+            tilt(shifted, (-1, 0));
+            tilt(shifted, (0, 1));
+            tilt(shifted, (1, 0));
+            plot(shifted);
+        }
+        if before_it == *shifted {
+            break;
+        }
+    }
+    //plot(&shifted);
+
+    let mut sum = 0;
+    for shifted in shifted.iter() {
+        for y in 0..shifted.len() {
+            for x in 0..shifted[0].len() {
+                if shifted[y][x] == 'O' {
+                    sum += shifted.len() - y;
+                }
+            }
+        }
+    }
+
+    let part2 = sum;
     dbg!(&part2);
-    //61669
-
     Ok(())
 }
