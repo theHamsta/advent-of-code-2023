59c0c3e66cfc936f14d89e799258df86acaac356
Stephan Seitz <sseitz@nvidia.com>
2023-12-17 09:04:10 +0100

    Find a general solution for both part2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -31,13 +31,7 @@ struct Pos {
     prev: Key,
 }
 
-fn main() -> anyhow::Result<()> {
-    let input = include_str!("../input");
-    //let input = include_str!("../example1");
-    //let input = include_str!("../example2");
-
-    let input = input.lines().map(|s| s.chars().collect_vec()).collect_vec();
-
+fn find_solution(input: &Vec<Vec<char>>, min_repetions: u32, max_repetitions: u32) -> Option<i64> {
     let mut pq = priority_queue::PriorityQueue::new();
 
     pq.push(
@@ -53,19 +47,12 @@ fn main() -> anyhow::Result<()> {
     let mut reached = HashMap::<Key, Pos>::new();
     let goal = (input[0].len() as i64 - 1, input.len() as i64 - 1);
 
-    dbg!(&goal);
-
-    let mut counter = 0;
     while let Some((current, _)) = pq.pop() {
         let (x, y) = current.pos;
-        counter += 1;
-        if counter % 10000 == 0 {
-            dbg!(&pq.len());
-        }
 
-        let key = (current.pos, current.repetitions.clone());
+        let key = (current.pos, current.repetitions);
         //let key = current.pos;
-        match reached.entry(key.clone()) {
+        match reached.entry(key) {
             std::collections::hash_map::Entry::Occupied(mut o) => {
                 if o.get().heat_loss > current.heat_loss {
                     o.insert(current.clone());
@@ -95,87 +82,79 @@ fn main() -> anyhow::Result<()> {
             let value = input[(y + dy) as usize][(x + dx) as usize] as u32 - '0' as u32;
             let heat_loss = current.heat_loss + value as i64;
             let mut repetitions = current.repetitions;
-            //dbg!(&(x, y));
-            //dbg!(&(dx, dy));
-            //dbg!(&repetitions.repetitions);
             if repetitions.dir == (dx, dy) || repetitions.dir == (0, 0) {
                 repetitions.repetitions += 1;
-                if repetitions.repetitions + 1 > 10 {
-                    //println!("too much rep");
+                if repetitions.repetitions + 1 > max_repetitions {
                     continue;
                 }
-                //println!("Same dir ok");
             } else {
-                if repetitions.repetitions + 1 < 4 {
-                    //println!("too few rep");
+                if repetitions.repetitions + 1 < min_repetions {
                     continue;
                 }
-                //println!("Turning");
                 repetitions.repetitions = 0;
             }
             repetitions.dir = (dx, dy);
-            //if last_three.dir ==
-            //if last_three.len() == 10 && last_three.iter().all(|&last| last == (dx, dy)) {
-            //continue;
-            //}
-            //last_three.push_back((dx, dy));
-            //if last_three.len() > 10 {
-            //last_three.pop_front();
-            //}
             pq.push(
                 Pos {
                     pos: new_pos,
                     repetitions,
                     heat_loss,
-                    prev: key.clone(),
+                    prev: key,
                 },
                 -heat_loss,
             );
-            //if new_pos == goal {
-            //break;
-            //}
         }
-        //1188
     }
 
     let last = reached
         .iter()
-        .filter_map(|((pos, reps), val)| (*pos == goal && reps.repetitions + 1 >= 4).then_some(val))
+        .filter_map(|((pos, reps), val)| {
+            (*pos == goal && reps.repetitions + 1 >= min_repetions).then_some(val)
+        })
         .min_by_key(|val| val.heat_loss);
-    //let last = &reached[&goal];
-    let part1 = last.map(|val| val.heat_loss);
-    dbg!(&part1);
-
     //if let Some(mut cur) = last {
-        //let mut prev: Option<&Pos> = None;
-        //let mut path = input.clone();
-        //loop {
-            //if let Some(prev) = prev {
-                //if prev.pos == cur.pos {
-                    //break;
-                //}
-                //path[cur.pos.1 as usize][cur.pos.0 as usize] =
-                    //match (prev.pos.0 - cur.pos.0, prev.pos.1 - cur.pos.1) {
-                        //(1, 0) => '>',
-                        //(-1, 0) => '<',
-                        //(0, 1) => 'V',
-                        //(0, -1) => '^',
-                        //_ => unreachable!(),
-                    //};
-            //}
-
-            //prev = Some(cur);
-            //cur = &reached[&cur.prev];
-        //}
-        //plot(&path);
+    //let mut prev: Option<&Pos> = None;
+    //let mut path = input.clone();
+    //loop {
+    //if let Some(prev) = prev {
+    //if prev.pos == cur.pos {
+    //break;
     //}
-    //1048
+    //path[cur.pos.1 as usize][cur.pos.0 as usize] =
+    //match (prev.pos.0 - cur.pos.0, prev.pos.1 - cur.pos.1) {
+    //(1, 0) => '>',
+    //(-1, 0) => '<',
+    //(0, 1) => 'V',
+    //(0, -1) => '^',
+    //_ => unreachable!(),
+    //};
+    //}
+
+    //prev = Some(cur);
+    //cur = &reached[&cur.prev];
+    //}
+    //plot(&path);
+    //}
 
     //let mut reached_field = input.clone();
     //for pos in reached.values() {
     //reached_field[pos.pos.1 as usize][pos.pos.0 as usize] = '#';
     //}
     //plot(&reached_field);
+    last.map(|val| val.heat_loss)
+}
+
+fn main() -> anyhow::Result<()> {
+    let input = include_str!("../input");
+    //let input = include_str!("../example1");
+    //let input = include_str!("../example2");
+
+    let input = input.lines().map(|s| s.chars().collect_vec()).collect_vec();
+
+    let part1 = find_solution(&input, 0, 3);
+    dbg!(&part1);
+    let part2 = find_solution(&input, 4, 10);
+    dbg!(&part2);
 
     Ok(())
 }
