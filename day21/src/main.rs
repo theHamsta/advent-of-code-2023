42f95110f8e8fbf3ae2ca7d25978587add0c608e
Stephan Seitz <sseitz@nvidia.com>
2023-12-21 07:32:01 +0100

    Part 1 naivve

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -2,18 +2,27 @@ use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
 
-type Point2d = (i16, i16);
+type PointInt = i64;
+type Point2d = (PointInt, PointInt);
 
 fn count_steps(
     input: &Vec<Vec<char>>,
     (x, y): Point2d,
     steps: i64,
     cache: &mut HashMap<(Point2d, i64), HashSet<Point2d>>,
+    infinite: bool,
 ) -> HashSet<Point2d> {
-    if x < 0
-        || x >= input[0].len() as i16
+    let y_lookup = y.rem_euclid(input.len() as i64);
+    let x_lookup = x.rem_euclid(input[0].len() as i64);
+
+    if infinite {
+        if input[y_lookup as usize][x_lookup as usize] == '#' {
+            return HashSet::new();
+        }
+    } else if x < 0
+        || x >= input[0].len() as i64
         || y < 0
-        || y >= input.len() as i16
+        || y >= input.len() as i64
         || input[y as usize][x as usize] == '#'
     {
         return HashSet::new();
@@ -25,20 +34,35 @@ fn count_steps(
         return rtn;
     }
     if let Some(res) = cache.get(&((x, y), steps)) {
-        return res.clone();
+        let lx = x - x_lookup;
+        let ly = y - y_lookup;
+        return res.iter().map(|(sx, sy)| (sx + lx, sy + ly)).collect();
+        //return res.clone();
     }
     let mut points = HashSet::new();
     for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
-        /*sum +=*/
-        points.extend(count_steps(input, (x + dx, y + dy), steps - 1, cache));
+        points.extend(count_steps(
+            input,
+            (x + dx, y + dy),
+            steps - 1,
+            cache,
+            infinite,
+        ));
     }
-    cache.insert(((x, y), steps), points.clone());
+    let lx = x - x_lookup;
+    let ly = y - y_lookup;
+    cache.insert(
+        ((x, y), steps),
+        points.iter().map(|(sx, sy)| (sx - lx, sy - ly)).collect(),
+    );
     points
 }
 
+
+
 fn main() -> anyhow::Result<()> {
     let input = include_str!("../input");
-    //let input = include_str!("../example1");
+    let input = include_str!("../example1");
 
     let input = input
         .lines()
@@ -50,17 +74,22 @@ fn main() -> anyhow::Result<()> {
     'outer: for y in 0..input.len() {
         for x in 0..input[0].len() {
             if input[y][x] == 'S' {
-                start = Some((x as i16, y as i16));
+                start = Some((x as i64, y as i64));
                 break 'outer;
             }
         }
     }
     let start = start.unwrap();
 
+    //let mut cache = HashMap::new();
+    //let points = count_steps(&input, start, 64, &mut cache, false);
+    //let part1 = points.len();
+    //dbg!(&part1);
+
     let mut cache = HashMap::new();
-    let points = count_steps(&input, start, 64, &mut cache);
-    let part1 = points.len();
-    dbg!(&part1);
+    let points = count_steps(&input, start, 500, &mut cache, true);
+    let part2 = points.len();
+    dbg!(&part2);
 
     Ok(())
 }
